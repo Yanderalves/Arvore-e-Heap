da2f5afb80510e55e1e6ae1b752c72f64d52914f
Yander alves <yanderceara@gmail.com>
2024-09-10 16:12:53 -0300

    feat: criando função de print, usando ponteiros e inicio do remover

--- main.c
+++ main.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 void subir(int i, int array[]);
-void construir(int array[], int n);
-void descer(int i, int n, int array[]);
+void construir(int *array, int *n);
+void descer(int i, int *n, int *array);
+// void remover();
+void print(int *array, int *n);
 
 int main()
 {
-  int array[] = {17, 8, 9, 6, 2, 21};
+  int n = 6;
+  // int *array = (int *)calloc(n, sizeof(int));
 
-  construir(array, 6);
+  // for (int i = 0; i < n; i++)
+  //{
+  //   array[i] = rand() % 1000;
+  // }
 
-  for (int i = 0; i < 6; i++)
-  {
-    printf("%d ->", array[i]);
-  }
+  int array[] = {17, 8, 21, 6, 2, 9};
+  construir(array, &n);
+
+  printf("Heap:");
+  print(array, &n);
+
   return 0;
 }
 
-void troca(int i, int j, int array[])
+void troca(int i, int j, int *array)
 {
   int aux = array[i];
   array[i] = array[j];
@@ -37,12 +46,12 @@ void subir(int i, int array[])
   }
 }
 
-void descer(int i, int n, int array[])
+void descer(int i, int *n, int *array)
 {
   int j = (2 * i) + 1;
-  if (j <= n)
+  if (j <= *n)
   {
-    if (j + 1 < n)
+    if (j + 1 < *n)
     {
       if (array[j + 1] > array[j])
       {
@@ -57,10 +66,34 @@ void descer(int i, int n, int array[])
   }
 }
 
-void construir(int array[], int n)
+void construir(int array[], int *n)
 {
-  for (int i = (n / 2) - 1; i >= 0; i--)
+  for (int i = (*n / 2) - 1; i >= 0; i--)
   {
     descer(i, n, array);
   }
+}
+
+// void remover(int *n, int *array)
+// {
+//   if (*n > 0)
+//   {
+//     array[0] = array[*n];
+//     *n = *n - 1;
+//     descer(0, n, array);
+//   }
+//   else
+//   {
+//     puts("Underflow");
+//   }
+// }
+
+void print(int *array, int *n)
+{
+  puts("");
+  for (int i = 0; i < *n; i++)
+  {
+    printf("%d ", array[i]);
+  }
+  puts("");
 }
